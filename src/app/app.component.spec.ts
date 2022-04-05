ef05e55ba6a7f52909fee728665a3af3efc1cc84
Mateusz <matjag102@op.pl>
2022-04-05 16:22:42 +0200

    naprawione

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zetofront'`, () => {
+  it(`should have as title 'zeto-fronted'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zetofront');
+    expect(app.title).toEqual('zeto-fronted');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zetofront app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('zeto-fronted app is running!');
   });
 });
