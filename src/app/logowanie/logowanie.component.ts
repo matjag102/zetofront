9bdd981db44c6e9dc0229b99be0a5b6a9c4ec957
Mateusz <matjag102@op.pl>
2022-04-06 22:26:43 +0200

    logowanie z tokenem

--- src/app/logowanie/logowanie.component.ts
+++ src/app/logowanie/logowanie.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-logowanie',
@@ -13,12 +16,49 @@ export class LogowanieComponent implements OnInit {
     haslo: new FormControl('', Validators.required)
   });
 
-  constructor() { }
+  loginData={
+    email: '',
+    haslo: '',
+  }
+  constructor(private _snackBar: MatSnackBar, private login:LoginService) { }
 
   ngOnInit(): void {
   }
 
-  loginUser(){
+  loginSubmit(){
+    console.log("submit")
     
+    if(this.loginData.email.trim()=='' || this.loginData.email.trim()==null){
+        this._snackBar.open("E-mail jest wymagany!",'',{
+          duration:3000,
+          verticalPosition: 'top'
+        });
+        return;
+    }
+    if(this.loginData.haslo.trim()=='' || this.loginData.haslo.trim()==null){
+      this._snackBar.open("Hasło jest wymagane!",'',{
+        duration:3000,
+        verticalPosition: 'top'
+      });
+      return;
+  }
+    //Server request
+    this.login.generateToken(this.loginData).subscribe(
+      //
+      (data)=>{
+        console.log("sukces");
+        console.log(data);
+        Swal.fire('Sukces!','Zostałeś zalogowany!', 'success');
+      },
+      (error)=>{
+        console.log(error);
+        this._snackBar.open("Coś poszło nie tak! Spróbuj ponownie!", "Ok",{
+          duration: 3000,
+          verticalPosition: 'top'
+        })
+      }
+
+      //
+    );
   }
 }
---
+++ src/app/service/login.service.ts
@@ -0,0 +1,71 @@
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  //generowanie tokenu
+
+  public generateToken(loginData: any){
+    return this.http.post(`${baseUrl}/generate-token`, loginData);
+  }
+  //LOGIN USER local storage
+  public loginUser(token:any){
+    localStorage.setItem("token", token);
+    return true
+  }
+
+  //czy zalogowany
+  public isLoggedIn(){
+    let tokenStr=localStorage.getItem("token");
+    if(tokenStr==undefined || tokenStr=='' || tokenStr==null)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
+  // wylogowanie
+  public logout()
+  {
+    localStorage.removeItem("token");
+    localStorage.removeItem("uzytkownik");
+    return true;
+  }
+
+  // get token
+  public getToken(){
+    return localStorage.getItem("token");
+  }
+
+  // szczegoly usera
+  public setUser(user:any){
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+
+  // get user
+  public getUser(){
+    let userStr=localStorage.getItem("user");
+    if(userStr!=null){
+      return JSON.parse(userStr);
+    }
+    else{
+      this.logout();
+      return null;
+    }
+  }
+  // pobieranie roli
+  //public getUserRole(){
+    //let user=this.getUser()
+    //return
+  //}
+}
