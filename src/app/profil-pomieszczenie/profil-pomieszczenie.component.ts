a52bbe3477572bf3a5ae37ca385b08b71720effb
matjag102 <matjag102@gmail.com>
2022-06-08 19:03:09 +0200

    style

--- src/app/profil-pomieszczenie/profil-pomieszczenie.component.ts
+++ src/app/profil-pomieszczenie/profil-pomieszczenie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import Swal from 'sweetalert2';
 import { SalaService } from '../service/sala.service';
@@ -8,6 +8,8 @@ import {CalendarModule} from 'primeng/calendar';
 import { RezerwacjaComponent } from '../rezerwacja/rezerwacja.component';
 import { RezerwacjaService } from '../service/rezerwacja.service';
 import {MatCalendarCellClassFunction, MatCalendarCellCssClasses} from '@angular/material/datepicker';
+import { STRING_TYPE } from '@angular/compiler'
+
 
 @Component({
   selector: 'app-profil-pomieszczenie',
@@ -25,14 +27,19 @@ export class ProfilPomieszczenieComponent implements OnInit {
   //from=new Date();
   //to=new Date();
   //j=0;
-  minD="";
+  
   rezerwacje=[]
 
   zarezerwowane=[]
 
-  kolizja=false;
+  kolizja=0
+  datesToHighlight = ["2019-01-22T18:30:00.000Z", "2019-01-22T18:30:00.000Z", "2019-01-24T18:30:00.000Z", "2019-01-28T18:30:00.000Z", "2019-01-24T18:30:00.000Z", "2019-01-23T18:30:00.000Z", "2019-01-22T18:30:00.000Z", "2019-01-25T18:30:00.000Z"];
 
-  
+  komunikat=""
+
+  minD=new Date();
+
+  minD2=new Date();
 
 
   idPomieszczenie=0;
@@ -63,20 +70,28 @@ export class ProfilPomieszczenieComponent implements OnInit {
       imageAlt:'sala6'
     },
   ]
+  myDateFilter = (d: Date): boolean => {
+    const day = d.getDay();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6 ;
+  }
 
 
   ngOnInit(): void {
-    this.minD=new Date().toDateString()
+    this.minD=new Date();
+    this.minD2=new Date();
     this.idPomieszczenie = this._route.snapshot.params['idPomieszczenie'];
     this.dane.id=this.idPomieszczenie;
     // alert(this.idPomieszczenie);
     this._sala.szczegoly(this.idPomieszczenie).subscribe(
       (data:any)=>{
         this.szczegoly=data;
-        console.log(data);
+        //console.log(data);
       },
       (error)=>{
-        console.log(error);
+        //console.log(error);
         Swal.fire('Błąd','Błąd podczas ładowania strony','error');
       }
       )
@@ -88,7 +103,8 @@ export class ProfilPomieszczenieComponent implements OnInit {
       this._rezerwacja.getRezerwacjePom(this.idPomieszczenie).subscribe(
         (data:any)=>{
           this.rezerwacje=data;
-          console.log(data);
+          //console.log(data);
+          let j=0;
           for(var i=0; i<this.rezerwacje.length;i++){
             let from=new Date(this.rezerwacje[i].dataStart);
             let to=new Date(this.rezerwacje[i].dataStop);
@@ -98,10 +114,14 @@ export class ProfilPomieszczenieComponent implements OnInit {
             //console.log(this.from)
             while(from<=to){
               year=from.getFullYear();
-              month=from.getMonth()+1;
-              day=from.getDate();
-              nowadata=year+'/'+month+'/'+day;
-              this.zarezerwowane.push(nowadata);
+              month=from.getMonth();
+              day=from.getDate()+1;
+              //nowadata=year+'-'+month+'-'+day;
+              nowadata=new Date(year,month,day)
+              this.zarezerwowane[j]=nowadata;
+              //console.log(this.zarezerwowane[j]);
+              j++;
+
               from.setDate(from.getDate()+1)
 
             }
@@ -114,20 +134,69 @@ export class ProfilPomieszczenieComponent implements OnInit {
         }
         );
 
+  }
+  onChangeEvent(){
+    //console.log(this.zarezerwowane[0])
+      this.kolizja=0;
+      let from=new Date(this.dane.dataStart);
+      let to=new Date(this.dane.dataStop);
+      from.setDate(from.getDate()+1)
+      to.setDate(to.getDate()+1)
+      
+      let year: any;
+      let month: any
+      let day: any;
+
+      let year1: any;
+      let month1: any
+      let day1: any;
+      
+
+      let year2: any;
+      let month2: any
+      let day2: any;
+
+      let tmp1: any;
+      let tmp2: any;
+      let i=0;
+      //console.log(from)
+      while(from<=to){
+        year1=from.getFullYear()
+        month1=from.getMonth()
+        day1=from.getDate()
+        tmp1=new Date(year1,month,day);
+        //console.log("test2"+tmp1)
+        for(let i=0;i<this.zarezerwowane.length;i++){
+          year2=this.zarezerwowane[i].getFullYear()
+          month2=this.zarezerwowane[i].getMonth()
+          day2=this.zarezerwowane[i].getDate()
+          tmp2=new Date(year2,month2,day2)
+          //console.log("->"+tmp2)
+          
+          if(tmp1.getTime()==tmp2.getTime()){
+            console.log(tmp1+"TESTOWANE"+tmp2)
+            this.kolizja=this.kolizja+1;
+          }
+          
+        }
+        year=from.getFullYear();
+        month=from.getMonth();
+        day=from.getDate()+1;
+        from.setDate(from.getDate()+1)
         
 
-
-  
-  }
-  dodajRezerwacje(){
-    console.log(this.dane.dataStart);
-    console.log(this.dane.dataStop);
-    for(var i=0; i<=this.zarezerwowane.length; i++){
-      if(this.zarezerwowane[i]>=this.dane.dataStart && this.zarezerwowane[i]<=this.dane.dataStop){
-        this.kolizja=true;
       }
+      console.log(this.kolizja)
     }
-    if(this.kolizja==false){
+
+  dodajRezerwacje(){
+    if(this.kolizja!=0){
+      this.komunikat="kolizja"
+    }
+    else{
+    //console.log(this.dane.dataStart);
+    //console.log(this.dane.dataStop);
+    this.komunikat=""
     this._rezerwacja.addRezerwacja(this.dane).subscribe(
       (data)=>{
        Swal.fire('Gotowe', 'dodano rezerwacje','success');
@@ -135,17 +204,24 @@ export class ProfilPomieszczenieComponent implements OnInit {
           id: 0,
           dataStop: new Date(),
           dataStart: new Date(),
+          
         }
+        window.location.reload()
       },
      (error)=>{
        Swal.fire('Bład','Bład podczas dodawania rezerwacji','error');
       }
     )
     }
-    else{
-      alert("kolizja")
-    }
+    
   }
- 
+
+  zmianamin(){
+    this.minD2=this.dane.dataStart;
+    //console.log(this.zarezerwowane[0].getDate())
+  }
+
+  
+
 
 }
